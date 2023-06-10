ca9200c2614ceb416fd1265277d4822a0d02105a
darryl west <dpw@raincitysoftware.com>
2023-06-10 13:36:08 -0700

    digit updates

--- digits.py
+++ digits.py
@@ -10,31 +10,66 @@ from collections import namedtuple
 
 Pair = namedtuple('Pair', 'a b')
 Factors = namedtuple('Factors', 'target pairs')
-Game = namedtuple('Game', 'target numbers factors')
+Game = namedtuple('Game', 'target digits factors')
 
+def parse_args():
+    game = Game(131, [2,3,7,9,11,25], [])
+    # game = Game(271, [3,4,6,7,8,11], [])
+
+    return game
+    
 def factor(target):
     pairs = []
 
-    for n in range(10):
-        pair = Pair(n, n*n)
-        pairs.append(pair)
+    x = 2
+    while (x < target):
+        if (target % x == 0):
+            pair = Pair(x, target // x)
+            
+            if (pair.a <= pair.b):
+                pairs.append(pair)
+
+        x = x + 1
 
     factors = Factors(target, pairs)
 
     return factors
 
-def parse_args():
-    game = Game(271, [3,4,6,7,8,11], None)
-    print(f'game: {game}')
+def show_pairs(pairs):
+    lst = []
+    for p in pairs:
+        # print(p)
+        lst.append(f'{p.a} * {p.b}')
 
-    return game
+    return lst
+        
+
+def show_factors(target, digits, factors):
+    print(target, factors.target, (target - factors.target))
+    for pair in factors.pairs:
+        if (pair.a in digits):
+            lst = show_pairs(factor(pair.b).pairs)
+            print(f"+ {pair.a} * {pair.b}", lst)
+        elif (pair.b in digits):
+            print(f"  {pair.a} * {pair.b} +")
+        else:
+            print(f"  {pair.a} * {pair.b}")
+
+
+def show_game(game):
+    print(f'Digits: {game.target}: {game.digits}' )
+    for f in game.factors:
+        show_factors(game.target, game.digits, f)
     
 def main():
     # parse to get the startup game
     game = parse_args()
 
-    factors = factor(game.target)
-    print(f'factors: {factors}')
+    for n in game.digits:
+        game.factors.append(factor(game.target + n))
+        game.factors.append(factor(game.target - n))
+
+    show_game(game)
 
 if __name__ == '__main__':
     main()
