7d07e6b11b5470ff41bdb52716fcfb5b6204bde1
darryl west <dpw@raincitysoftware.com>
2023-06-12 08:08:23 -0700

    convert to toml

---
+++ convert-to-toml.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# dpw@plaza.localdomain
+# 2023-06-12 14:53:12
+
+import toml
+import json
+
+with open('games.json', 'r') as jsonfile:
+    data = json.load(jsonfile)
+
+data = {'data': data}
+toml_str = toml.dumps(data)
+
+with open('games.toml', 'w') as tomlfile:
+    tomlfile.write(toml_str)
+
