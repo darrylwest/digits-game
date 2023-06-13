fd4559f8c35bc0798f4f69ae71dc7122a40b6f9e
darryl west <dpw@raincitysoftware.com>
2023-06-13 07:35:41 -0700

    ui impl

--- web/js/app.js
+++ web/js/app.js
@@ -1,12 +1,27 @@
-const APP_VERSION = "1.0"
+const APP_VERSION = "0.1.2"
 
-// namespace
+// define the components
+const submitButton = document.querySelector('#submit-button');
+const targetInput = document.querySelector('#targetInput');
+const numbersInput = document.querySelector('#numbersInput');
+const resultOutput = document.querySelector('#resultOutput');
+const footerMessage = document.querySelector('#footerMessage');
+
+
+// application namespace
 const app = { };
 
 app.version = () => APP_VERSION;
 
 app.post = (target, numbers) => {
     console.log("post the target/numbers", target, numbers);
+
+    setTimeout(() => {
+        console.log("posting")
+        resultOutput.innerHTML = "Result: 5*3+16*3";
+        submitButton.disabled = false;
+        submitButton.value = "Solve"
+    }, 1000);
 };
 
 app.parse_target = (value) => {
@@ -25,11 +40,6 @@ app.parse_numbers = (value) => {
 
     return numbers;
 };
-
-// define the components
-const submitButton = document.querySelector('#submit-button');
-const targetInput = document.querySelector('#targetInput');
-const numbersInput = document.querySelector('#numbersInput');
 
 submitButton.addEventListener('click', () => {
     console.log("submit button click");
@@ -50,3 +60,4 @@ submitButton.addEventListener('click', () => {
 
 });
 
+footerMessage.innerHTML = "Copyright (c) 2023, Version: " + APP_VERSION;
